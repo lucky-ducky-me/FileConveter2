bb985c6f401c185e08d1825672e9f8bb62a5d89d
lucky-ducky-me <vlad01ovchinnikov02@gmail.com>
2022-11-22 18:46:08 +0300

    Добавлена работа с командной строкой и проверочные данные.

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import FileConverter.*;
+import Weapon.*;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Main {
+
+    static public void main(String[] args) throws ParserConfigurationException, IOException {
+
+        try {
+
+            var path = Paths.get("");
+
+            var sourceFile = path.toAbsolutePath().toString() + "\\src\\main\\resources\\test.xml";
+
+            var destinationFile = path.toAbsolutePath().toString() + "\\src\\main\\resources\\test.json";
+
+            if (args != null && args.length >= 2) {
+                sourceFile = args[0];
+
+                destinationFile = args[1];
+            }
+            
+            if (sourceFile.endsWith(".xml") && destinationFile.endsWith(".json")) {
+                FileConverterService.xmlToJson(sourceFile, destinationFile);
+            }
+            else if (sourceFile.endsWith(".json") && destinationFile.endsWith(".xml")) {
+                FileConverterService.jsonToXml(destinationFile, sourceFile);
+            }
+            else {
+                throw new Exception("Переданные файлы неверных типов.");
+            }
+        }
+        catch (Exception ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
